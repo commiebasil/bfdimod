a01ceef21aac8766a0732cc86a7b28814a298836
DeveloperPun <punkid211@outlook.com>
2025-06-14 13:52:08 -0500

    New Raid and focus changes

--- common/defines/bfdi_defines.lua
+++ common/defines/bfdi_defines.lua
@@ -1,4 +1,4 @@
 NDefines.NGame.START_DATE = "2011.12.1.12"
 NDefines.NGame.END_DATE = "2050.1.1.1"
 NDefines.NGame.GAME_SPEED_SECONDS = { 2.0, 0.5, 0.2, 0.1, 0.02 }
-NDefines.NDiplomacy.TENSION_DECAY_DAILY = 0.002
+NDefines.NDiplomacy.TENSION_DECAY_DAILY = 0.001
